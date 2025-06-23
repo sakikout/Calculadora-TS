5a160d863aefd71a7e7be6debeaac8eb236dc2f1
sakikout <bedalfior@gmail.com>
2025-06-23 13:43:59 -0300

    Update style.ts

--- calculadora-ts/src/components/styles.ts
+++ calculadora-ts/src/components/styles.ts
@@ -1,17 +1,41 @@
 import styled from 'styled-components';
 
+interface ButtonProps {
+  $colSpan?: number;
+  $rowSpan?: number;
+}
+
+export const Grid = styled.div`
+    display: flex;
+    flex-direction: row;
+
+`;
+
+export const HistoryContainer = styled.div`
+    
+    width: 200px;
+    margin: 100px auto;
+    background: #0E1116;
+    padding: 20px;
+`;
+
+export const HistoryTitle = styled.h2`
+    list-style-type: none;
+    font-size: 1.2rem;
+`;
+
 export const CalculatorContainer = styled.div`
-  width: 300px;
+  width: 400px;
   margin: 100px auto;
-  background: #1e1e1e;
+  background: #0E1116;
   padding: 20px;
   border-radius: 12px;
   box-shadow: 0 8px 20px rgba(0, 0, 0, 0.3);
 `;
 
 export const Display = styled.div`
-  background: #000;
-  color: #0f0;
+  background: #252D4A;
+  color:rgb(255, 255, 255);
   font-size: 2rem;
   padding: 10px;
   border-radius: 8px;
@@ -22,12 +46,13 @@ export const Display = styled.div`
 export const ButtonGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 60px; 
   gap: 10px;
 `;
 
-export const Button = styled.button`
-  background: #333;
-  color: #fff;
+export const Button = styled.button<ButtonProps>`
+  background: #181C2A;
+  color: rgb(255, 255, 255);
   font-size: 1.5rem;
   padding: 15px;
   border: none;
@@ -35,11 +60,15 @@ export const Button = styled.button`
   cursor: pointer;
   transition: background 0.2s;
 
+    ${({ $colSpan }) => $colSpan && `grid-column: span ${$colSpan};`}
+    ${({ $rowSpan }) => $rowSpan && `grid-row: span ${$rowSpan};`}
+
   &:hover {
-    background: #555;
+    background:rgb(54, 63, 99);
   }
 
   &:active {
     background: #777;
   }
 `;
+
